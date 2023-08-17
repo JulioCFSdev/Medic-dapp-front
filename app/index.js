a2fd788c686a0eb9a5aff3766b84cb4d726fcd84
NatanSIsantos <55513395+NatanSIsantos@users.noreply.github.com>
2023-08-17 01:09:55 -0400

    Metamask Integration
    Creates metamask integration for medics and patients

--- app/index.js
+++ app/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, TouchableOpacity, Image } from 'react-native';
+import { View, Text, TouchableOpacity, Image, Platform, Linking } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import SplashScreen from 'react-native-splash-screen';
@@ -12,10 +12,64 @@ import { Welcome } from '../components';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { ScrollView } from 'react-native-gesture-handler';
 import styles from '../components/home/welcome/welcome.style';
+import { debug } from 'react-native-reanimated';
 
 const Stack = createStackNavigator();
 
 const HomeScreen = ({ navigation }) => {
+  const handleMedicoLogin = async () => {
+    if (typeof window.ethereum !== 'undefined') {
+      try {
+        await window.ethereum.request({ method: 'eth_requestAccounts' });
+        // Usuário fez login com MetaMask, redirecionar para MedicoScanner
+        navigation.navigate('MedicoScanner');
+      } catch (error) {
+        // Usuário não fez login ou negou a solicitação, exibir aviso
+        console.log('Usuário não fez login com MetaMask');
+        // Você pode exibir um aviso aqui usando um componente ou um modal
+      }
+    } else {
+      // MetaMask não está instalado, exibir aviso
+      console.log('MetaMask não está instalado');
+      {renderWebWarning()}
+      // Você pode exibir um aviso aqui usando um componente ou um modal
+    }
+  };
+
+  const handlePacienteLogin = async () => {
+    if (typeof window.ethereum !== 'undefined') {
+      try {
+        await window.ethereum.request({ method: 'eth_requestAccounts' });
+        // Usuário fez login com MetaMask, redirecionar para a tela de paciente
+        navigation.navigate('Patient');
+      } catch (error) {
+        // Usuário não fez login ou negou a solicitação, exibir aviso
+        console.log('Usuário não fez login com MetaMask');
+        // Você pode exibir um aviso aqui usando um componente ou um modal
+      }
+    } else {
+      // MetaMask não está instalado, exibir aviso
+      console.log('MetaMask não está instalado');
+      {renderWebWarning()}
+      // Você pode exibir um aviso aqui usando um componente ou um modal
+    }
+  };
+
+  const WebRedirect = () => {
+    console.log("Entrou aqui")
+    // Redirecionar para o site oficial do MetaMask
+    Linking.openURL('https://metamask.io/');
+  };
+
+  const renderWebWarning = () => {
+    if (Platform.OS === 'web') {
+      return (
+        WebRedirect()
+      );
+    }
+    return null;
+  };
+
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
       <ScrollView showsVerticalScrollIndicator={false}>
@@ -29,13 +83,13 @@ const HomeScreen = ({ navigation }) => {
 
           <Welcome></Welcome>
 
-          <TouchableOpacity style={styles.searchBtn} onPress={() => navigation.navigate('MedicoScanner')}>
+          <TouchableOpacity style={styles.searchBtn}  onPress={handleMedicoLogin}>
             <Text style={{ color: 'white' }}>Médico</Text>
           </TouchableOpacity>
 
           <TouchableOpacity
             style={styles.searchBtn2}
-            onPress={() => navigation.navigate('Patient')}
+            onPress={handlePacienteLogin}
           >
             <Text style={{ color: 'white' }}>Paciente</Text>
           </TouchableOpacity>
