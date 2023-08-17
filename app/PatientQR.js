7ba95a6ed876b977624ed81bd2ddfe9ea713d838
Debby-Barros <jmlds.snf21@uea.edu.br>
2023-08-16 21:11:12 -0400

    Patient QR generator

--- app/PatientQR.js
+++ app/PatientQR.js
@@ -1,18 +1,23 @@
 import react from "react";
 import { View, Text, TouchableOpacity, TextInput, Image, FlatList } from "react-native";
 import QRCode from 'react-native-qrcode-svg';
+import { COLORS } from '../constants';
+import styles from "../components/home/welcome/welcome.style";
 
 
 const QR = ({ navigation }) => {
-    const data = 'https://chat.openai.com/c/bf1db05b-0973-41b1-b141-156c70352490';
+    const data = 'https://open.spotify.com/intl-pt/album/2rWL18jMYe1D6JsE4CHSF6?si=DAt6hoSgQ1ujmpaE0U7BoQ';
 
 
     return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: COLORS.lightWhite }}>
+            <Text style={styles.welcomeMessage}>Escaneie o QRCode Abaixo</Text>
             <QRCode
                 value={data}
+                size={200} // Tamanho do QR code
+                color="black" // Cor dos módulos do QR code
+                backgroundColor="white" // Cor de fundo do QR code
             />
-            <Text>Scan QR Code</Text>
         </View>
     )
 }
