04203c5a25f392a85826e9138190870c0e3e2c56
NatanSIsantos <55513395+NatanSIsantos@users.noreply.github.com>
2023-08-28 19:29:06 -0400

    QR Code Reader

--- app/MedicoScanner.js
+++ app/MedicoScanner.js
@@ -1,68 +1,118 @@
-import React, { useEffect, useState } from "react";
-import { View, Text, TouchableOpacity, Platform } from "react-native";
+import React, { useState, useEffect } from "react";
+import { View, Text, TextInput, TouchableOpacity } from "react-native";
 import { COLORS } from '../constants';
 import styles from "../components/home/welcome/welcome.style";
-
-import { BarCodeScanner } from 'expo-barcode-scanner';
+import { jsQR } from "jsqr";
 
 const Scanner = ({ navigation }) => {
-    const [hasPermission, setHasPermission] = useState(null);
+    const jsQR = require("jsqr");
     const [scanned, setScanned] = useState(false);
-    const [text, setText] = useState('Coloque a Câmera do seu dispositivo sobre o código QR');
+    const [text, setText] = useState('insira o código do paciente abaixo ou coloque o QR Code na câmera');
+    const [inputText, setInputText] = useState(''); // Novo estado para o input de texto
 
-    const askForCameraPermission = () => {
-        (async () => {
-            const { status } = await BarCodeScanner.requestPermissionsAsync();
-            setHasPermission(status == 'granted');
-        })();
+    const handleScan = (code) => {
+        if (code) {
+            setScanned(true);
+            setText(code.data);
+            console.log('Data:', code.data);
+
+            if (isValidUrl(code.data)) {
+                window.open(code.data, '_blank');
+            }
+        } else {
+            console.error("Não foi possível decodificar o código QR");
+        }
+    }
+
+    const handleScanAgain = () => {
+        setScanned(false);
+        setText('Coloque a Câmera do seu dispositivo sobre o código QR');
+    }
+
+    const handleSend = () => {
+        if (inputText.trim() !== '') {
+            // Trate o texto inserido pelo usuário
+            console.log("Texto inserido:", inputText);
+        }
+    }
+
+    const isValidUrl = (url) => {
+        try {
+            new URL(url);
+            return true;
+        } catch (error) {
+            return false;
+        }
     }
 
     useEffect(() => {
-        askForCameraPermission();
+        const videoElement = document.createElement('video');
+        videoElement.id = 'video';
+        document.body.appendChild(videoElement);
+
+        const constraints = {
+            video: true,
+        };
+
+        navigator.mediaDevices.getUserMedia(constraints)
+            .then((stream) => {
+                videoElement.srcObject = stream;
+
+                const canvasElement = document.createElement('canvas');
+                const canvasContext = canvasElement.getContext('2d');
+
+                const scan = () => {
+                    if (videoElement.readyState === videoElement.HAVE_ENOUGH_DATA) {
+                        canvasElement.width = videoElement.videoWidth;
+                        canvasElement.height = videoElement.videoHeight;
+                        canvasContext.drawImage(videoElement, 50, 0, canvasElement.width, canvasElement.height);
+
+                        const imageData = canvasContext.getImageData(0, 0, canvasElement.width, canvasElement.height);
+                        const code = jsQR(imageData.data, imageData.width, imageData.height, {
+                            inversionAttempts: "dontInvert",
+                        });
+
+                        if (code) {
+                            handleScan(code);
+                        }
+
+                        requestAnimationFrame(scan);
+                    } else {
+                        requestAnimationFrame(scan);
+                    }
+                };
+
+                videoElement.play();
+                requestAnimationFrame(scan);
+            })
+            .catch((error) => {
+                console.error('Erro ao acessar a câmera:', error);
+            });
+
+        return () => {
+            const videoElement = document.getElementById('video');
+            if (videoElement) {
+                videoElement.srcObject.getTracks().forEach(track => track.stop());
+                document.body.removeChild(videoElement);
+            }
+        };
     }, []);
-
-    const handleBarCodeScanned = ({ type, data }) => {
-        setScanned(true);
-        setText(data);
-        console.log('Type: ' + type + '\nData ' + data)
-    }
-
-    if (hasPermission === null) {
-        return (
-            <View style={styles.container}>
-                <Text>Aguardando Permissão da Câmera</Text>
-            </View>
-        )
-    }
-
-    if (hasPermission === false) {
-        return (
-            <View style={styles.container}>
-                <Text style={{ margin: 10 }}>Acesso Negado</Text>
-                <TouchableOpacity title={'Permitir Acesso'} onPress={() => askForCameraPermission()}></TouchableOpacity>
-            </View>
-        )
-    }
-
-    // Verificar se o ambiente é a web
-    if (Platform.OS === 'web') {
-        return (
-            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: COLORS.lightWhite }}>
-                <Text>Leitura de QR Code não é suportada na web.</Text>
-            </View>
-        )
-    }
 
     return (
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: COLORS.lightWhite }}>
-            <BarCodeScanner
-                onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
-                style={{ height: 400, width: 400, marginLeft: -55, marginTop: -200 }}
+            <Text style={{ marginTop: 20 }}>{text}</Text>
+
+            {scanned && <TouchableOpacity title={'Escanear Novamente?'} onPress={handleScanAgain} />}
+
+            <TextInput
+                style={{ marginTop: 20, paddingHorizontal: 10, width: 200, borderColor: 'gray', borderWidth: 1 }}
+                placeholder="Insira o código do paciente..."
+                value={inputText}
+                onChangeText={setInputText}
             />
-            <Text style={{ marginTop: 50 }}>{text}</Text>
-
-            {scanned && <TouchableOpacity title={'Escanear Novamente?'} onPress={() => setScanned(false)} />}
-
+            <TouchableOpacity title={'Enviar'} onPress={handleSend}>
+                <Text>Enviar</Text>
+            </TouchableOpacity>
         </View>
     )
 }
